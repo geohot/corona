cbb089f6fae697752b33f688a79b523905e1efde
George Hotz <geohot@gmail.com>
2020-04-02 00:48:14 -0700

    opencl

--- fold.py
+++ fold.py
@@ -5,6 +5,9 @@ from simtk.unit import *
 from sys import stdout
 import os
 
+#platform = Platform.getPlatformByName("CUDA")
+platform = Platform.getPlatformByName("OpenCL")
+
 protein_pdb = "proteins/villin/1vii.pdb"
 pdb = PDBFile(protein_pdb)
 forcefield = ForceField('amber99sb.xml', 'tip3p.xml')
@@ -14,7 +17,7 @@ print(modeller.topology)
 
 system = forcefield.createSystem(modeller.topology, nonbondedMethod=NoCutoff, nonbondedCutoff=1*nanometer, constraints=HBonds)
 integrator = LangevinIntegrator(300*kelvin, 1/picosecond, 0.002*picoseconds)
-simulation = Simulation(modeller.topology, system, integrator)
+simulation = Simulation(modeller.topology, system, integrator, platform)
 simulation.context.setPositions(modeller.positions)
 simulation.minimizeEnergy()
 simulation.reporters.append(PDBReporter('/tmp/output.pdb', 25))
