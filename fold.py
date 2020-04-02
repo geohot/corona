f022ef908e9ba5cb13ca28ac3226ae2bb89f1bfa
George Hotz <geohot@gmail.com>
2020-04-02 16:42:30 -0700

    we have args

--- fold.py
+++ fold.py
@@ -4,23 +4,33 @@ from simtk.openmm import *
 from simtk.unit import *
 from sys import stdout
 import os
+import sys
+import argparse
+
+parser = argparse.ArgumentParser(description='Fold some proteins.')
+parser.add_argument('--scratch', action='store_true')
+parser.add_argument('--steps', type=int, default=100000, help="2500000000 should fold the protein")
+parser.add_argument('--writes', type=int, default=1000, help="default is 1000")
+parser.add_argument('--out', type=str, default="/tmp/output.pdb")
+args = parser.parse_args(sys.argv[1:])
 
 try:
   platform = Platform.getPlatformByName("CUDA")
 except Exception:
   platform = Platform.getPlatformByName("OpenCL")
 
-# already folded protein
-#protein_pdb = "proteins/villin/1vii.pdb"
-#pdb = PDBFile(protein_pdb)
-
-# unfolded protein
-protein_fasta = "proteins/villin/1vii.fasta"
-fasta = open(protein_fasta).read().split("\n")[1]
-print("folding %s" % fasta)
-from lib import write_unfolded
-write_unfolded(fasta, "/tmp/unfolded.pdb")
-pdb = PDBFile("/tmp/unfolded.pdb")
+if args.scratch:
+  # unfolded protein
+  protein_fasta = "proteins/villin/1vii.fasta"
+  fasta = open(protein_fasta).read().split("\n")[1]
+  print("folding %s" % fasta)
+  from lib import write_unfolded
+  write_unfolded(fasta, "/tmp/unfolded.pdb")
+  pdb = PDBFile("/tmp/unfolded.pdb")
+else:
+  # already folded protein
+  protein_pdb = "proteins/villin/1vii.pdb"
+  pdb = PDBFile(protein_pdb)
 
 forcefield = ForceField('amber99sb.xml', 'tip3p.xml')
 
@@ -34,15 +44,10 @@ simulation = Simulation(modeller.topology, system, integrator, platform)
 simulation.context.setPositions(modeller.positions)
 simulation.minimizeEnergy()
 
-#steps = 100000
-# 2500000000
-
-# still off by factor of 100!
-steps = 25000000
-
-steps_write = steps//1000
+steps = args.steps
+steps_write = steps//args.writes
 print("writing every %d steps" % steps_write)
-simulation.reporters.append(PDBReporter('/tmp/output.pdb', steps_write))
+simulation.reporters.append(PDBReporter(args.out, steps_write))
 simulation.reporters.append(StateDataReporter(stdout, steps_write, step=True, potentialEnergy=True, temperature=True))
 simulation.step(steps)
 
