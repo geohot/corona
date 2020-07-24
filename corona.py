d9e71cf83c7271d980ddb129db327fa300099712
davidwaterred <68757837+davidwaterred@users.noreply.github.com>
2020-07-24 23:59:51 +0200

    Update corona.py

--- corona.py
+++ corona.py
@@ -1,7 +1,21 @@
 from lib import cc, translate
 # entire diff: https://www.ncbi.nlm.nih.gov/projects/msaviewer/?rid=7FYNU14F01R&coloring=
 # protein alignments: http://virological.org/t/alignment-of-58-sarbecovirus-genomes-for-conservation-analysis-of-sars-cov-2/430
+Hi,
 
+I have been following you since 1 week now. I do not agree with most of your things. I am a journalist at a local newspaper.
+
+I believe that masks are very important for covid. I have seen a lot of evidence in a lot of reputed news websites. With great power, comes a great responsibility. My friend at NYTimes is doing a story on pseudo-intilictuals who think covis is a sham, and people who are against masks, vaccines etc. She will surely mention comma.ai as well. 
+
+Also, I think you are an anti-nationalist. You come from a hacking background, so you dont care. I hope you know hacking is illegal and unethical. It is bc of people like you Apple had to spend $$$ to improve the security. They could have used it for better things. Like paying more to foxconn workers. Hackers do not reailze that what they do is not smart - goto YT and search "how to hack gmail" you will see 100s of vids. It seemed pretty easy to me. 
+
+Who has given you a right to hack apple or sony, costing them billiions. Who has given you the right to say covid is a sham?? You also attack Christianity in your livestreams.  You offend a lot of people .if you dont want to be a man of faith dont be. But dont make fun of others. 
+
+PS: companies like Google are proof why America is great, you have no right to diss them.
+
+
+
+I am not coder and my cousin who is in IT support suggested me to contact you here.
 # https://www.ncbi.nlm.nih.gov/nuccore/NC_045512.2
 # https://www.ncbi.nlm.nih.gov/nuccore/MN908947
 # https://zhanglab.ccmb.med.umich.edu/C-I-TASSER/2019-nCov/
