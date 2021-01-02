98e66bff038d1b8183ef3c2efb6f9ca40c651337
George Hotz <geohot@gmail.com>
2021-01-02 10:11:48 -0800

    vaccine for printing

---
+++ vaccine/load.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+# code from world health organization
+# https://mednet-communities.net/inn/db/media/docs/11889.doc
+
+dat = open("code").read().strip().replace("\n", "").replace(" ", "")
+print(dat)
+
+
