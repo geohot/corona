43a3d078db5ea218a09ecbffc41875e60c7408bd
George Hotz <geohot@gmail.com>
2021-01-02 10:24:18 -0800

    load

--- vaccine/load.py
+++ vaccine/load.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python3
 
+import pathlib
+import os
+
 # code from world health organization
 # https://mednet-communities.net/inn/db/media/docs/11889.doc
 
-dat = open("code").read().strip().replace("\n", "").replace(" ", "")
-print(dat)
+with open(os.path.join(pathlib.Path(__file__).parent.absolute(), "code")) as f:
+  dat = f.read().strip().replace("\n", "").replace(" ", "")
+
+if __name__ == "__main__":
+  print(len(dat))
+  print(dat)
+
 
 
