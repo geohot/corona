4617e4c8693fff77c455b89efaaa2a28fbbcf896
haezurath <44448302+haezurath@users.noreply.github.com>
2021-03-17 21:23:06 -0400

    Update fold.py

--- fold.py
+++ fold.py
@@ -7,20 +7,16 @@ import os
 import sys
 import argparse
 
-class Fold:
 
-  def __init__(self):
-    self.parser = argparse.ArgumentParser(description='Fold some proteins.')
-    self.parser.add_argument('--scratch', action='store_true')
-    self.parser.add_argument('--temp', type=int, default=300)
-    self.parser.add_argument('--steps', type=int, default=100000, help="2500000000 should fold the protein")
-    self.parser.add_argument('--writes', type=int, default=1000, help="default is 1000")
-    self.parser.add_argument('--out', type=str, default="/tmp/output.pdb")
-    self.parser.add_argument('--pdb', type=str, default="proteins/villin/1vii.pdb")
-    self.parser.add_argument('--fasta', type=str, default=None)
-
-a = Fold()
-args = a.parser.parse_args(sys.argv[1:])
+parser = argparse.ArgumentParser(description='Fold some proteins.')
+parser.add_argument('--scratch', action='store_true')
+parser.add_argument('--temp', type=int, default=300)
+parser.add_argument('--steps', type=int, default=100000, help="2500000000 should fold the protein")
+parser.add_argument('--writes', type=int, default=1000, help="default is 1000")
+parser.add_argument('--out', type=str, default="/tmp/output.pdb")
+parser.add_argument('--pdb', type=str, default="proteins/villin/1vii.pdb")
+parser.add_argument('--fasta', type=str, default=None)
+args = parser.parse_args(sys.argv[1:])
 
 try:
   platform = Platform.getPlatformByName("CUDA")
