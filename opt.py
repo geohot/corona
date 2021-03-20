6fa3cd373a448b5b4e8325d05d3846c9dbf53c28
eldoonnihs <solangm@my.yorku.ca>
2021-03-20 14:59:05 -0400

    changed the corona virus sequence in opt.py such that it is now a rna_sequence object from corona.py

--- opt.py
+++ opt.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 from lib import cc as virus
 from vaccine.load import dat as vaccine
-from corona import corona
+from corona import *
 
-virus = virus.replace("T", "U")
+# virus = virus.replace("T", "U")
 vaccine = vaccine.replace("Ψ", "U")
 
 """
@@ -17,7 +17,14 @@ exit(0)
 
 # vaccine starts at 21508 with a 67% match
 # spike protein starts at 21562
-vvirus = virus[21508:21508+len(vaccine)]
+# vvirus = virus[21508:21508+len(vaccine)]
+
+# changed the RNA sequence of the corona virus where we have the overlap with the vaccine 
+# so that it is an rna_sequence object from corona.py so that this can have the full access to the method of the rna_sequence object 
+
+vaccine_coverage = rna_sequence('vaccine_coverage', cc[21508:21508+len(vaccine)], True)
+vvirus = vaccine_coverage.rna_sequence_range.replace("T", "U")
+
 
 print(vvirus)
 print(vaccine)
