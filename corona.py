931c1ef269ced8a2ca9b28e3ecc4851378b03656
RomanKova <kovalchykrv@gmail.com>
2021-04-16 08:56:53 +0530

    Update corona.py

--- corona.py
+++ corona.py
@@ -1,4 +1,4 @@
-from lib import cc, translate
+from lib import genome, translate
 # entire diff: https://www.ncbi.nlm.nih.gov/projects/msaviewer/?rid=7FYNU14F01R&coloring=
 # protein alignments: http://virological.org/t/alignment-of-58-sarbecovirus-genomes-for-conservation-analysis-of-sars-cov-2/430
 
@@ -44,15 +44,17 @@ corona = {}
 #        https://en.wikipedia.org/wiki/MRNA_(nucleoside-2%27-O-)-methyltransferase
 
 # in front "the untranslated leader sequence that ends with the Transcription Regulation Sequence"
-corona['untranslated_region'] = cc[0:265]
+corona['untranslated_region'] = genome.get_nucleotideSequence()[0:265]
 
-corona['orf1a'] = translate(cc[266-1:13483], True)
+corona['orf1a'] = translate(genome.get_nucleotideSequence()[266-1:13483], True)
 
 # cc[266-1+4398*3:13468] = 'TTT_TTA_AAC' aka 'X_XXY_YYZ'
 # https://en.wikipedia.org/wiki/Ribosomal_frameshift
 # Programmed −1 Ribosomal Frameshifting
 # TODO: add this to the translate function with automatic detection
-corona['orf1b'] = translate(cc[13468-1:21555], False).strip("*")  # chop off the stop, note this doesn't have a start
+# chop off the stop, note this doesn't have a start
+corona['orf1b'] = translate(genome.get_nucleotideSequence()[
+                            13468-1:21555], False).strip("*")
 
 # exploit vector, this attaches to ACE2. also called "surface glycoprotein"
 # https://www.ncbi.nlm.nih.gov/Structure/pdb/6VYB -- open state
@@ -62,28 +64,37 @@ corona['orf1b'] = translate(cc[13468-1:21555], False).strip("*")  # chop off the
 #   S2  = 686-1273
 #   S2' = 816-1273
 # https://www.ncbi.nlm.nih.gov/pmc/articles/PMC2750777/
-corona['spike_glycoprotein'] = translate(cc[21563-1:25384], True)
+corona['spike_glycoprotein'] = translate(
+    genome.get_nucleotideSequence()[21563-1:25384], True)
 
 # Forms homotetrameric potassium sensitive ion channels (viroporin) and may modulate virus release.
-corona['orf3a'] = translate(cc[25393-1:26220], True)
+corona['orf3a'] = translate(
+    genome.get_nucleotideSequence()[25393-1:26220], True)
 
 # these two things stick out, used in assembly aka they package the virus
-corona['envelope_protein'] = translate(cc[26245-1:26472], True)  # also known as small membrane
-corona['membrane_glycoprotein'] = translate(cc[26523-1:27191], True)
+corona['envelope_protein'] = translate(
+    genome.get_nucleotideSequence()[26245-1:26472], True)  # also known as small membrane
+corona['membrane_glycoprotein'] = translate(
+    genome.get_nucleotideSequence()[26523-1:27191], True)
 
-corona['orf6'] = translate(cc[27202-1:27387], True)
+corona['orf6'] = translate(
+    genome.get_nucleotideSequence()[27202-1:27387], True)
 
-corona['orf7a'] = translate(cc[27394-1:27759], True)
-corona['orf7b'] = translate(cc[27756-1:27887], True)  # is this one real?
+corona['orf7a'] = translate(
+    genome.get_nucleotideSequence()[27394-1:27759], True)
+corona['orf7b'] = translate(genome.get_nucleotideSequence()[
+                            27756-1:27887], True)  # is this one real?
 
-corona['orf8'] = translate(cc[27894-1:28259], True)
+corona['orf8'] = translate(
+    genome.get_nucleotideSequence()[27894-1:28259], True)
 
 # https://en.wikipedia.org/wiki/Capsid
 # Packages the positive strand viral genome RNA into a helical ribonucleocapsid
 # Includes the "internal" protein (from Coronavirus Pathogenesis)
 # https://www.sciencedirect.com/topics/veterinary-science-and-veterinary-medicine/human-coronavirus-oc43
-corona['nucleocapsid_phosphoprotein'] = translate(cc[28274-1:29533], True)
+corona['nucleocapsid_phosphoprotein'] = translate(
+    genome.get_nucleotideSequence()[28274-1:29533], True)
 
 # might be called the internal protein (Coronavirus Pathogenesis)
-corona['orf10'] = translate(cc[29558-1:29674], True)
-
+corona['orf10'] = translate(
+    genome.get_nucleotideSequence()[29558-1:29674], True)
